21ec2ef7a838fa40f21143c16e08566b529e2001
BangerTech <73241309+BangerTech@users.noreply.github.com>
2023-08-02 10:35:44 +0200

    changed box size

--- setup_script_x86.sh
+++ setup_script_x86.sh
@@ -14,9 +14,9 @@ CHOICES=$(whiptail --backtitle "BangerTECH INSTALLATION SCRIPT X86 VERSION" --ti
   "node-exporter" "Data Export used to show host stats in Grafana" OFF  3>&1 1>&2 2>&3)
 
 if [ -z "$CHOICES" ]; then
-  whiptail --title "MESSAGE" --msgbox "No option was selected (user hit Cancel or ESC)" 8 120
+  whiptail --title "MESSAGE" --msgbox "No option was selected (user hit Cancel or ESC)" 8 82
   else
-  if whiptail --title "CONFIRMATION" --yesno "You are about to install: $CHOICES" 8 120; then 
+  if whiptail --title "CONFIRMATION" --yesno "You are about to install: $CHOICES" 8 82; then 
     for CHOICE in $CHOICES; do
     case "$CHOICE" in
       '"openHAB"')
@@ -27,10 +27,10 @@ if [ -z "$CHOICES" ]; then
         sudo ln -s /opt/openhabian/openhabian-setup.sh /usr/local/bin/openhabian-config
         sudo cp /opt/openhabian/build-image/openhabian.conf /etc/openhabian.conf
         sudo openhabian-config unattended
-        if whiptail --title "MESSAGE" --yesno "Would you like to restore your old openHAB config?" 8 120; then
+        if whiptail --title "MESSAGE" --yesno "Would you like to restore your old openHAB config?" 8 82; then
         sudo openhab-cli restore /var/lib/openhab/backups/openhab-backup.zip
         else 
-          whiptail --title "MESSAGE" --msgbox "OK enjoy using openHAB" 8 120
+          whiptail --title "MESSAGE" --msgbox "OK enjoy using openHAB" 8 82
         fi
       ;;
       '"Docker"')
@@ -61,14 +61,14 @@ if [ -z "$CHOICES" ]; then
         mkdir ~/docker-compose-data/zigbee2mqtt && cd ~/docker-compose-data/zigbee2mqtt
         sudo wget https://raw.github.com/BangerTech/zigbee2mqtt/main/docker-compose.yml
         sudo docker-compose up -d
-        whiptail --title "Zigbee2MQTT" --msgbox "Check your Zigbee Network here http://yourip:7000" 8 120
+        whiptail --title "Zigbee2MQTT" --msgbox "Check your Zigbee Network here http://yourip:7000" 8 82
       ;;
       '"Homebridge"')
         mkdir ~/docker-compose-data && cd ~/docker-compose-data
         mkdir ~/docker-compose-data/homebridge && cd ~/docker-compose-data/homebridge
         sudo wget https://raw.github.com/BangerTech/homebridge/main/docker-compose.yml
         sudo docker-compose up -d
-        whiptail --title "Homebridge" --msgbox "Setup the HomeKit Bridge here http://yourip:8321" 8 120
+        whiptail --title "Homebridge" --msgbox "Setup the HomeKit Bridge here http://yourip:8321" 8 82
       ;;
       '"Grafana"')
         mkdir ~/docker-compose-data && cd ~/docker-compose-data
@@ -77,21 +77,21 @@ if [ -z "$CHOICES" ]; then
         mkdir ~/docker-compose-data/grafana/data && cd ~/docker-compose-data/grafana/data
         sudo wget https://raw.github.com/BangerTech/grafana/main/env.grafana
         sudo docker-compose up -d
-        whiptail --title "Grafana" --msgbox "The Dashboard´s are located here http://yourip:3000" 8 120
+        whiptail --title "Grafana" --msgbox "The Dashboard´s are located here http://yourip:3000" 8 82
       ;;
       '"influxDB"')
         mkdir ~/docker-compose-data && cd ~/docker-compose-data
         mkdir ~/docker-compose-data/influxdb && cd ~/docker-compose-data/influxdb
         sudo wget https://raw.github.com/BangerTech/influxdb-x86/main/docker-compose.yml
         sudo docker-compose up -d
-        whiptail --title "influxDB" --msgbox "You´ll find the WebUI on port http://yourip:8086" 8 120
+        whiptail --title "influxDB" --msgbox "You´ll find the WebUI on port http://yourip:8086" 8 82
       ;;
       '"Portainer"')
         mkdir ~/docker-compose-data && cd ~/docker-compose-data
         mkdir ~/docker-compose-data/portainer && cd ~/docker-compose-data/portainer
         sudo wget https://raw.github.com/BangerTech/portainer/main/docker-compose.yml
         sudo docker-compose up -d
-        whiptail --title "Portainer" --msgbox "You´ll find the WebUI on port http://yourip:8999" 8 120
+        whiptail --title "Portainer" --msgbox "You´ll find the WebUI on port http://yourip:8999" 8 82
       ;;
       '"node-exporter"')
         mkdir ~/docker-compose-data && cd ~/docker-compose-data
@@ -105,12 +105,12 @@ if [ -z "$CHOICES" ]; then
       ;;
       esac
     done
-      if whiptail --title "MESSAGE" --yesno "PACKAGES: $CHOICES installed successfully.\nWould you like to reboot?" 8 120; then
+      if whiptail --title "MESSAGE" --yesno "PACKAGES: $CHOICES installed successfully.\nWould you like to reboot?" 8 82; then
         sudo reboot
       else 
-        whiptail --title "MESSAGE" --msgbox "All Done!" 8 120
+        whiptail --title "MESSAGE" --msgbox "All Done!" 8 82
       fi
   else
-    whiptail --title "MESSAGE" --msgbox "Cancelling Process since user pressed <NO>." 8 120
+    whiptail --title "MESSAGE" --msgbox "Cancelling Process since user pressed <NO>." 8 82
   fi
 fi

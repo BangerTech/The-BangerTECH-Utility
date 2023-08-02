6e8a5cb5fc298037d8bbdc084c8d71fb645c3bc2
BangerTech <73241309+BangerTech@users.noreply.github.com>
2023-08-02 18:58:53 +0200

    Update and rename setup_script_arm.sh to bangertech_utility_arm.sh

--- bangertech_utility_arm.sh
+++ bangertech_utility_arm.sh
@@ -2,7 +2,7 @@
 
 sudo apt update && sudo apt upgrade -y
 
-CHOICES=$(whiptail --backtitle "BangerTECH INSTALLATION SCRIPT ARM VERSION" --title "SELECT PACKAGES TO INSTALL"  --checklist "Choose options" 22 82 15 \
+CHOICES=$(whiptail --backtitle "The BangerTECH Utility ARM VERSION" --title "SELECT PACKAGES TO INSTALL"  --checklist "Choose options" 22 82 10 \
   "openHAB" "install openHABian on top of your running System " ON \
   "Docker" "install just the Docker Engine" OFF \
   "Docker+Docker-Compose" "install Docker & Docker-Compose" OFF \
@@ -19,9 +19,9 @@ CHOICES=$(whiptail --backtitle "BangerTECH INSTALLATION SCRIPT ARM VERSION" --ti
   "node-exporter" "Data Export used to show host stats in Grafana" OFF  3>&1 1>&2 2>&3)
 
 if [ -z "$CHOICES" ]; then
-  whiptail --title "MESSAGE" --msgbox "No option was selected (user hit Cancel or ESC)" 8 82
+  whiptail --backtitle "The BangerTECH Utility ARM VERSION" --title "MESSAGE" --msgbox "No option was selected (user hit Cancel or ESC)" 8 82
   else
-  if whiptail --title "CONFIRMATION" --yesno "You are about to install: $CHOICES" 8 82; then 
+  if whiptail --backtitle "The BangerTECH Utility ARM VERSION" --title "CONFIRMATION" --yesno "You are about to install: $CHOICES" 8 82; then 
     for CHOICE in $CHOICES; do
     case "$CHOICE" in
       '"openHAB"')
@@ -31,10 +31,10 @@ if [ -z "$CHOICES" ]; then
         sudo ln -s /opt/openhabian/openhabian-setup.sh /usr/local/bin/openhabian-config
         sudo cp /opt/openhabian/build-image/openhabian.conf /etc/openhabian.conf
         sudo openhabian-config unattended
-        if whiptail --title "MESSAGE" --yesno "Would you like to restore your old openHAB config?" 8 82; then
+        if whiptail --backtitle "The BangerTECH Utility ARM VERSION" --title "MESSAGE" --yesno "openHAB is running on port http://yourIP:8080\nWould you like to restore your old openHAB config?" 8 82; then
         sudo openhab-cli restore /var/lib/openhab/backups/openhab-backup.zip
         else 
-          whiptail --title "MESSAGE" --msgbox "OK enjoy using openHAB" 8 82
+          whiptail --backtitle "The BangerTECH Utility ARM VERSION" --title "MESSAGE" --msgbox "OK enjoy using openHAB" 8 82
         fi
       ;;
       '"Docker"')
@@ -68,14 +68,14 @@ if [ -z "$CHOICES" ]; then
         sudo mkdir -p $HOME/docker-compose-data/zigbee2mqtt && cd $HOME/docker-compose-data/zigbee2mqtt
         sudo wget -nc https://raw.github.com/BangerTech/zigbee2mqtt/main/docker-compose.yml
         sudo docker-compose up -d
-        whiptail --title "Zigbee2MQTT" --msgbox "Check your Zigbee Network here http://yourip:7000" 8 82
+        whiptail --backtitle "The BangerTECH Utility ARM VERSION" --title "Zigbee2MQTT" --msgbox "Check your Zigbee Network here http://yourIP:7000" 8 82
       ;;
       '"Homebridge"')
         sudo mkdir -p $HOME/docker-compose-data && cd $HOME/docker-compose-data
         sudo mkdir -p $HOME/docker-compose-data/homebridge && cd $HOME/docker-compose-data/homebridge
         sudo wget -nc https://raw.github.com/BangerTech/homebridge/main/docker-compose.yml
         sudo docker-compose up -d
-        whiptail --title "Homebridge" --msgbox "Setup the HomeKit Bridge here http://yourip:8321" 8 82
+        whiptail --backtitle "The BangerTECH Utility ARM VERSION" --title "Homebridge" --msgbox "Setup the HomeKit Bridge here http://yourIP:8321" 8 82
       ;;
       '"Grafana"')
         sudo mkdir -p $HOME/docker-compose-data && cd $HOME/docker-compose-data
@@ -84,21 +84,21 @@ if [ -z "$CHOICES" ]; then
         sudo mkdir -p $HOME/docker-compose-data/grafana/data && cd $HOME/docker-compose-data/grafana/data
         sudo wget -nc https://raw.github.com/BangerTech/grafana/main/env.grafana
         sudo docker-compose up -d
-        whiptail --title "Grafana" --msgbox "The Dashboard´s are located here http://yourip:3000" 8 82
+        whiptail --backtitle "The BangerTECH Utility ARM VERSION" --title "Grafana" --msgbox "The Dashboard´s are located here http://yourIP:3000" 8 82
       ;;
       '"influxDB"')
         sudo mkdir -p $HOME/docker-compose-data && cd $HOME/docker-compose-data
         sudo mkdir -p $HOME/docker-compose-data/influxdb && cd $HOME/docker-compose-data/influxdb
         sudo wget -nc https://raw.github.com/BangerTech/influxdb-arm/main/docker-compose.yml
         sudo docker-compose up -d
-        if whiptail --title "MESSAGE" --yesno "Would you like to create a DATABASE openhab with USER openhabuser ?" 8 82; then
+        if whiptail --backtitle "The BangerTECH Utility ARM VERSION" --title "MESSAGE" --yesno "Would you like to create a DATABASE openhab with USER openhabuser ?" 8 82; then
         sudo docker exec -it influxdb /usr/bin/influx
         CREATE DATABASE openhab
         USE openhab
         CREATE USER openhabuser with PASSWORD 'openhab' WITH ALL PRIVILEGES
         GRANT ALL PRIVILEGES ON openhab TO openhabuser
         else 
-          whiptail --title "MESSAGE" --msgbox "You need to create your own DATABASE & USER " 8 82
+          whiptail --backtitle "The BangerTECH Utility ARM VERSION" --title "MESSAGE" --msgbox "You need to create your own DATABASE & USER " 8 82
         fi
       ;;
       '"Portainer"')
@@ -106,7 +106,7 @@ if [ -z "$CHOICES" ]; then
         sudo mkdir -p $HOME/docker-compose-data/portainer && cd $HOME/docker-compose-data/portainer
         sudo wget -nc https://raw.github.com/BangerTech/portainer/main/docker-compose.yml
         sudo docker-compose up -d
-        whiptail --title "Portainer" --msgbox "You´ll find the WebUI on port http://yourip:8999" 8 82
+        whiptail --backtitle "The BangerTECH Utility ARM VERSION" --title "Portainer" --msgbox "You´ll find the WebUI on port http://yourIP:8999" 8 82
       ;;
       '"Filebrowser"')
         sudo mkdir -p $HOME/docker-compose-data && cd $HOME/docker-compose-data
@@ -117,28 +117,28 @@ if [ -z "$CHOICES" ]; then
         sudo wget -nc https://raw.github.com/BangerTech/filebrowser/main/settings.json
         sudo wget -nc https://raw.github.com/BangerTech/filebrowser/main/docker-compose.yml
         sudo docker-compose up -d
-        whiptail --title "Filebrowser" --msgbox "You´ll find your self hosted Filebrowser on port http://yourip:8998" 8 82
+        whiptail --backtitle "The BangerTECH Utility ARM VERSION" --title "Filebrowser" --msgbox "You´ll find your self hosted Filebrowser on port http://yourIP:8998" 8 82
       ;;
       '"Heimdall"')
         sudo mkdir -p $HOME/docker-compose-data && cd $HOME/docker-compose-data
         sudo mkdir -p $HOME/docker-compose-data/heimdall && cd $HOME/docker-compose-data/heimdall
         sudo wget -nc https://raw.github.com/BangerTech/heimdall/main/docker-compose.yml
         sudo docker-compose up -d
-        whiptail --title "Heimdall" --msgbox "You´ll find the Dashboard on port http://yourip:8500" 8 82
+        whiptail --backtitle "The BangerTECH Utility ARM VERSION" --title "Heimdall" --msgbox "You´ll find the Dashboard on port http://yourIP:8500" 8 82
       ;;
       '"HomeAssistant"')
         sudo mkdir -p $HOME/docker-compose-data && cd $HOME/docker-compose-data
         sudo mkdir -p $HOME/docker-compose-data/homeassistant && cd $HOME/docker-compose-data/homeassistant
         sudo wget -nc https://raw.github.com/BangerTech/homeassistant/main/docker-compose.yml
         sudo docker-compose up -d
-        whiptail --title "HomeAssistant" --msgbox "Your HomeAssistant is located here http://yourip:8123" 8 82
+        whiptail --backtitle "The BangerTECH Utility ARM VERSION" --title "HomeAssistant" --msgbox "Your HomeAssistant is located here http://yourIP:8123" 8 82
       ;;
       '"CodeServer"')
         sudo mkdir -p $HOME/docker-compose-data && cd $HOME/docker-compose-data
         sudo mkdir -p $HOME/docker-compose-data/codeserver && cd $HOME/docker-compose-data/codeserver
         sudo wget -nc https://raw.github.com/BangerTech/codeserver/main/docker-compose.yml
         sudo docker-compose up -d
-        whiptail --title "CodeServer" --msgbox "Your CodeServer is located here http://yourip:8443\nPassword is: admin\nYou may change it here $HOME/docker-compose-data/codeserver/docker-compose.yml" 14 82
+        whiptail --backtitle "The BangerTECH Utility ARM VERSION" --title "CodeServer" --msgbox "Your CodeServer is located here http://yourIP:8443\nPassword is: admin\nYou may change it here $HOME/docker-compose-data/codeserver/docker-compose.yml" 14 82
       ;;
       '"node-exporter"')
         sudo mkdir -p $HOME/docker-compose-data && cd $HOME/docker-compose-data
@@ -153,12 +153,12 @@ if [ -z "$CHOICES" ]; then
       esac
       sudo apt autoremove -y
     done
-      if whiptail --title "MESSAGE" --yesno "PACKAGES: $CHOICES installed successfully.\nWould you like to reboot?" 8 82; then
+      if whiptail --backtitle "The BangerTECH Utility ARM VERSION" --title "MESSAGE" --yesno "PACKAGES: $CHOICES installed successfully.\nWould you like to reboot?" 8 82; then
         sudo reboot
       else 
-        whiptail --title "MESSAGE" --msgbox "All Done!" 8 82
+        whiptail --backtitle "The BangerTECH Utility ARM VERSION" --title "MESSAGE" --msgbox "All Done!" 8 82
       fi
   else
-    whiptail --title "MESSAGE" --msgbox "Cancelling Process since user pressed <NO>." 8 82
+    whiptail --backtitle "The BangerTECH Utility ARM VERSION" --title "MESSAGE" --msgbox "Cancelling Process since user pressed <NO>." 8 82
   fi
 fi

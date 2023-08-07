637ad372118c4d87536cc6ffb07630d8a2633fa2
BangerTECH <info@bangertech.de>
2023-08-07 12:43:28 +0200

    added logo

--- bangertech_utility_x86.sh
+++ bangertech_utility_x86.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y
-sudo apt install whiptail -y
+sudo bash -c 'sudo apt install whiptail -y >/dev/null 2>&1 & disown'
+sudo bash -c 'sudo apt update && sudo apt upgrade -y >/dev/null 2>&1 & disown'
+sudo wget sudo wget -nc https://raw.githubusercontent.com/BangerTech/The-BangerTECH-Utility/development/scripts/logo.txt
+sudo cat logo.txt
+echo "Website: https://bangertech.de"
+echo "Donations: https://www.paypal.com/donate/?hosted_button_id=FD26FHKRWS3US"
+
+#sudo apt update && sudo apt upgrade -y
+#sudo apt install whiptail -y
+
+sleep 5
 
 CHOICES=$(whiptail --backtitle "The BangerTECH Utility X86 VERSION" --title "SELECT PACKAGES TO INSTALL"  --checklist "Choose options" 26 85 18 \
   "openHAB" "install openHABian on top of your running System " ON \
